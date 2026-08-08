35c679c29a67d7a34084d95e4f2c81a3b4d84aa2
Tausifqureshi <tausifqureshi504@gmail.com>
2026-08-08 10:28:31 +0530

    Nodejs Asynchrinoius

---
+++ Introduction/introductions.js
@@ -0,0 +1,15 @@
+function introduce(name) {
+  console.log(`Hello, my name is ${name}`);
+  function introduce(name) {
+    console.log(`Hello, my name is ${name}`);
+  }
+  function introduce(name) {
+    console.log(`Hello, my name is ${name}`);
+  }
+}
+introduce("Alice");
+
+function introduce(name) {
+  console.log(`Hello, my name is ${name}`);
+}
+introduce("Bob");
