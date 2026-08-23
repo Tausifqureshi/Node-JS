ceabc8886b7e537a7236d79fe56b7f201c132ab4
Tausifqureshi <tausifqureshi504@gmail.com>
2026-08-23 10:43:45 +0530

    Fix: Corrected module path and function call in mainPractical.js

--- mainPractical.js
+++ mainPractical.js
@@ -1,1 +1,9 @@
-console.log("Server start your please check this file run")
+// console.log("Server start your please check this file run")
+const sum = require("./Modules_CommonJS_vs_ESM/calculator.js");
+
+const num1 = 50;
+const num2 = 10;
+
+// const total = sum.add(num1, num2);
+const total = sum.add(35,45);
+console.log("Total:", total);
